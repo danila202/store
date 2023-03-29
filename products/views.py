be1b93632031db89366bc3b479f5ca7b111fb4e8
Danya Dyakonov <danila-dyakonov@mail.ru>
2023-03-30 01:20:36 +0300

    adding TitleMixin

--- products/views.py
+++ products/views.py
@@ -6,34 +6,28 @@ from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from user.models import User
+from common.views import TitleMixin
 
-
-class IndexView(TemplateView):
+class IndexView(TitleMixin,TemplateView):
     template_name = 'products/index.html'
+    title = 'Store'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Store'
-        return context
 
-class ProductsListView(ListView):
+class ProductsListView(TitleMixin,ListView):
     model = Product
     template_name = 'products/products.html'
     paginate_by = 3
+    title = 'Store - Каталог'
 
     def get_queryset(self):
         queryset = super().get_queryset()
         category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id) if category_id else queryset
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_context_data(self):
         context = super().get_context_data()
-        context['title'] = 'Store - Каталог'
         context['categories'] = ProductCategory.objects.all()
         return context
-
-
-
 
 
 @login_required
