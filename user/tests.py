d8ce2c5dec18f033910cf0b76042cecdce3ccd70
Danya Dyakonov <danila-dyakonov@mail.ru>
2023-04-10 21:59:49 +0300

    Created test for registration

---
+++ user/tests.py
@@ -0,0 +1,60 @@
+from http import HTTPStatus
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+from datetime import timedelta
+
+from .models import User, EmailVerification
+
+
+class UserRegistrationTest(TestCase):
+
+    def setUp(self):
+        self.path = reverse('user:registration')
+        self.data = {
+            'first_name': 'Danilka',
+            'last_name': 'Dyakonov',
+            'username': 'mocroper13',
+            'email': 'danila-dyakonov@mail.ru',
+            'password1': '1q2w3e_VB',
+            'password2': '1q2w3e_VB'
+        }
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code,HTTPStatus.OK)
+        self.assertTemplateUsed(response,'user/registration.html'),
+        self.assertEqual(response.context_data['title'], 'Store - Регистрация')
+
+    def test_user_registration_post_success(self):
+        username = self.data.get('username')
+        self.assertFalse(User.objects.filter(username=username).exists())
+
+        response = self.client.post(self.path,self.data)
+
+        self.assertEqual(response.status_code,HTTPStatus.FOUND)
+        self.assertRedirects(response,reverse('user:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual((now() + timedelta(hours=24)).date(), email_verification.first().expiration.date())
+
+    def test_user_registration_post_error(self):
+        User.objects.create(username=self.data.get('username'))
+        response = self.client.post(self.path,self.data)
+
+        self.assertEqual(response.status_code,HTTPStatus.OK)
+        self.assertContains(response,'Пользователь с таким именем уже существует.',html=True)
+
+
+
+
+
+
+
+
+
+
+
